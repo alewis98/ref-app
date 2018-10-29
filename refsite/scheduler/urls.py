9aba75d223473470e1cded1f25f301ba8d4bb0c8
Andrew Lewis <anjale98@gmail.com>
2018-10-29 05:15:35 -0400

    moved everything

--- refsite/scheduler/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-#from django.contrib.auth.views import login, logout
-from .views import *
-
-app_name = 'home'
-urlpatterns = [
-    # Homepage
-    path('', HomeView.as_view(), name='home'),
-]
